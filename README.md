75075161b5ea18b5bc8b2f9483e4a14c3aacb834
Ricardo Tubio Pardavila <rtpardavila@gmail.com>
2019-02-04 17:39:50 +0900

    [master@20190204:173950] Basic example working

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # stellar-sandbox
 Sandbox for the StellarStation API.
+
+Steps to run the basic example included:
+
+* Install Python 3 and virtualenv (commands for Debian)
+
+  sudo apt update && sudo apt install python3 python3-virtualenv
+
+* Create virtualenv:
+
+  virtualenv --python python3 .stellar
+
+* Load virtualenv:
+
+  activate .stellar/bin/activate
+
+* Install python dependencies:
+
+  pip install -r requirements.txt
+
+* Create private keys directory within $HOME:
+
+  mkdir -p "$HOME/.infostellar"
+
+* Download private key from console and store it under "$HOME/.infostellar"
+* Return to repository's root and execute the basic example that should list
+the services available:
+
+  python examples/basic.py
+
+The class provided within "basic.py" can be used as an initial example of how
+to connect a more complex client to StellarStation's API.
---
+++ examples/basic.py
@@ -0,0 +1,232 @@
+# Copyright 2018 Infostellar, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The Python implementation of the gRPC stellarstation-api client."""
+
+
+import grpc
+import inspect
+import logging
+import os
+from pprint import pformat
+import time
+
+from google.auth import jwt as gauth_jwt
+from google.auth.transport import grpc as gauth_grpc
+from stellarstation.api.v1 import stellarstation_pb2
+from stellarstation.api.v1 import stellarstation_pb2_grpc
+
+os.environ['GRPC_SSL_CIPHER_SUITES'] = 'ECDHE-RSA-AES128-GCM-SHA256'
+
+
+def createBitstreamSatelliteChannel(satellite_id):
+    """Factory
+    This method creates a satellite channel object that exchanges bitstream
+    chunks of data in a bidirectional fashion. Users are responsible for the
+    correct serialization / deserialization of the packets through the stream.
+    """
+    return SatelliteChannel(
+        satellite_id, stellarstation_pb2.Framing.Value('BITSTREAM')
+    )
+
+
+def createAX25SatelliteChannel(satellite_id):
+    """Factory
+    This method creates a satellite channel object that exchanges AX25 packets
+    in between both ends.
+    """
+    return SatelliteChannel(
+        satellite_id, stellarstation_pb2.Framing.Value('AX25')
+    )
+
+
+def createIQSatelliteChannel(satellite_id):
+    """Factory
+    This method creates a satellite channel object that exchanges IQ data in
+    between both ends.
+    """
+    return SatelliteChannel(
+        satellite_id, stellarstation_pb2.Framing.Value('IQ')
+    )
+
+
+def createPngSatelliteChannel(satellite_id):
+    """Factory
+    This method creates a streaming channel that permits receiving the images
+    of the decoded data coming from certain weather satellites.
+    """
+    return SatelliteChannel(
+        satellite_id, stellarstation_pb2.Framing.Value('IMAGE_PNG')
+    )
+
+
+class SatelliteChannel(object):
+    """
+    This class creates a bidirectional communications channel with an existing
+    satellite using StellarStation's API.
+    """
+
+    def __init__(
+        self, satellite_id, framing,
+        url='https://api.stellarstation.com',
+        channel_url='api.stellarstation.com:443'
+    ):
+        """Constructor
+        Constructs a satellite channel object that connects to the satellite
+        identified by the given paramenter. The satellite needs to be
+        registered within StellarStation's platform and accessible to the user
+        whose credentials are used for the connection. For a given user, the
+        satellites that can be accessed through the API are listed in the
+        console.
+
+        @param satellite_id -- identifier of the satellite to connect to
+        @param framing -- identifier that defines the protocol running on top
+        @param url -- URL of StellarStation's API
+        @param channel_url -- service definition for DNS resolution
+        """
+        self._l = logging.getLogger(self.__class__.__name__)
+        self._l.setLevel(logging.DEBUG)
+
+        self.satellite_id = satellite_id
+        self.framing = framing
+        self._url = url
+        self._channel_url = channel_url
+
+        self._createSecureChannel()
+
+    def _createSecureChannel(self):
+        """
+        This method creates a client for StellarStation's API.
+        """
+        home = os.path.expanduser('~')
+        key = os.path.join(home, '.infostellar/stellarstation-private-key.json')
+
+        credentials = gauth_jwt.Credentials.from_service_account_file(
+            key, audience=self._url
+        )
+        jwt_creds = gauth_jwt.OnDemandCredentials.from_signing_credentials(
+            credentials
+        )
+        self.channel = gauth_grpc.secure_authorized_channel(
+            jwt_creds, None, self._channel_url
+        )
+
+        self.client = stellarstation_pb2_grpc.StellarStationServiceStub(
+            self.channel
+        )
+
+        self._l.info(
+            'Connected to (%s), satellite = %s, protocol = %s',
+            self._url,
+            self.satellite_id,
+            stellarstation_pb2.Framing.Name(self.framing)
+        )
+
+    def _createRequest(self):
+        """
+        This method creates a request for the satellite stream.
+        """
+        yield stellarstation_pb2.SatelliteStreamRequest(
+            satellite_id=str(self.satellite_id),
+            accepted_framing=[self.framing]
+        )
+
+    def getStream(self):
+        """
+        This method returns a data stream for satellite telemetry read.
+        """
+        self.request = self._createRequest()
+        self.stream = self.client.OpenSatelliteStream(self.request)
+        return self.stream
+
+    def logTelemetry(self, wait=True, retryTime=5):
+        """
+        This method leaves the client listening for data coming from the
+        satellite through the open stream. The data is logged into the default
+        logging service from the Python library.
+
+        # notice # This is an example of how to use this class for waiting for
+                    frames.
+        # notice # For connecting with an external service, it is recommended
+                    to retrieve the stream through "getStream()" and to iterate
+                    over it like it is done in this function.
+        """
+        self.getStream()
+
+        while wait:
+
+            try:
+                self._l.debug('Waiting for frames to arrive...')
+                for telemetry in self.stream:
+                    self._l.info('telemetry = <%s>', telemetry)
+            except Exception as ex:
+                self._l.warn('Exception while waiting for TM, msg = %s', ex)
+
+            time.sleep(retryTime)
+
+    def printServices(self):
+        """
+        This method logs the available API services (methods / classes).
+        """
+        self._services = {c[0]: c[1]
+            for c in inspect.getmembers(
+                stellarstation_pb2, predicate=inspect.isclass
+            )
+        }
+
+        self._gprcServices = [m
+            for m in dir(self.client)
+            if callable(getattr(self.client, m)) and not m.startswith('__')
+        ]
+
+        self._l.info('Callable items within the API:')
+        self._l.info('API available classes = \n%s', pformat(self._services))
+        self._l.info('GRPC methods = \n%s', pformat(self._gprcServices))
+
+    def sendTelecommand(self, tcArray):
+        """
+        This method sends an array of telecommands through the open stream.
+
+        @param tcArray -- array of telecommands to be sent, each of them needs
+                            to be a bytearray object
+        """
+        self._l.debug('Telecommands array request')
+
+        command_request = stellarstation_pb2.SendSatelliteCommandsRequest(
+            output_framing=self.framing, command=tcArray
+        )
+        satellite_stream_request = stellarstation_pb2.SatelliteStreamRequest(
+            satellite_id=self.satellite_id,
+            send_satellite_commands_request=command_request
+        )
+
+        yield satellite_stream_request
+        self._l.debug('Telecommands array sent')
+
+
+if __name__ == '__main__':
+
+    # 61 is an example of an identifier for a satellite.
+    # The class provided in this example is suppossed to be used like this:
+    satellite = createBitstreamSatelliteChannel(61)
+
+    # The following method can be called to print the available classes and
+    # methods in the API.
+    satellite.printServices()
+
+    # The following method can be used to send telecommands to the satellite
+    # satellite.sendTelecommand([b'05050505'])
+
+    # The following method waits for telemetry frames and logs their content.
+    satellite.logTelemetry()
